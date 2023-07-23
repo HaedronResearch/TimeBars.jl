1bd8a6fc2496380315079676967c44eb76755458
kevindirect <git@kevindirect.com>
2023-07-22 22:55:21 -0700

    renames

--- src/SeriesBars.jl
+++ src/SeriesBars.jl
@@ -7,13 +7,14 @@ using Transducers
 using DocStringExtensions: TYPEDEF, TYPEDFIELDS, TYPEDSIGNATURES
 
 include("bar/abstract/bar.jl")
-include("bar/abstract/indexed.jl")
-include("bar/abstract/series.jl")
-include("bar/abstract/timeseries.jl")
-include("bar/abstract/timetype.jl")
-include("bar/concrete/ohlc.jl")
+include("bar/abstract/indexedbar.jl")
+include("bar/abstract/seriesbar.jl")
+include("bar/abstract/timeseriesbar.jl")
+include("bar/abstract/timetypebar.jl")
+include("bar/concrete/ohlcbar.jl")
 
 export TimeTypeBar
+export OHLCBar
 export index, lag, lead, isregular, downsample
 
 end
