7aa0b36377d53f4c28e3b1ff64d2bec8ef33cd92
kevindirect <git@kevindirect.com>
2023-07-22 23:46:35 -0700

    style

--- src/SeriesBars.jl
+++ src/SeriesBars.jl
@@ -6,15 +6,15 @@ using StructArrays
 using Transducers
 using DocStringExtensions: TYPEDEF, TYPEDFIELDS, TYPEDSIGNATURES
 
+export TimeTypeBar
+export OHLCBar
+export index, lag, lead, isregular, downsample
+
 include("bar/abstract/bar.jl")
 include("bar/abstract/indexedbar.jl")
 include("bar/abstract/seriesbar.jl")
 include("bar/abstract/timeseriesbar.jl")
 include("bar/abstract/timetypebar.jl")
 include("bar/concrete/ohlcbar.jl")
-
-export TimeTypeBar
-export OHLCBar
-export index, lag, lead, isregular, downsample
 
 end
